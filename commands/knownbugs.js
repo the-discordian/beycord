758cc2c1dd200f2b8d6add835b70727daa67c962
CorruptX <corrupt2112@gmail.com>
2021-04-30 23:29:59 -0500

    Fixed known bugs

--- commands/knownbugs.js
+++ commands/knownbugs.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args, prefix, player, db) => {
   let now = new Date();
   let startembed = new Discord.MessageEmbed()
   .setTitle('Known Bugs')
-  .setDescription('Below is a list of all the known bugs as of **May 1, 2021.** ```\nBeys do not have a valid ID/No ID/Undefined \nMultiple commands are turned off/disabled due to multiple issues in the code.```')
+  .setDescription('Below is a list of all the known bugs as of **May 1, 2021.** ```\n[1]Beys do not have a valid ID/No ID/Undefined \n \n[2]Multiple commands are turned off/disabled due to multiple issues in the code.```')
   .setColor("#7f7fff")
   .setTimestamp();
   message.channel.createMessage({embed:startembed});
