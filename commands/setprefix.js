d7cc01304cd1aae7c453f12b196cbe158bcec393
CorruptX <corrupt2112@gmail.com>
2021-08-02 14:57:52 -0500

    setprefix

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -2,16 +2,6 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (client, message, args, prefix, player, db) => {
-  let now = new Date();
-  let startembed = new Discord.MessageEmbed()
-  .setTitle('Disabled command')
-  .setDescription('This command is temporarily disabled due to maintenance. Join our [discord server!](https://discord.com/invite/42vkvZMv7d)')
-  .setColor("#fa2c2c")
-  .setTimestamp()
-  message.channel.createMessage({embed:startembed});
-}
-
-/*module.exports.run = async (client, message, args, prefix, player, db) => {
 
   if(!message.member.hasPermission("manageGuild")) return message.reply("buddy you didn't meet the requirement to use this command. Missing Permission: `MANAGE_SERVER`");
   if(!args[0] || args[0 == "help"]) return message.reply(`Usage: ${prefix}prefix <new prefix>`);
@@ -25,7 +15,7 @@ module.exports.run = async (client, message, args, prefix, player, db) => {
 
   message.channel.createMessage({embed:sEmbed});
 
-}*/
+}
 
 
 module.exports.help = {
