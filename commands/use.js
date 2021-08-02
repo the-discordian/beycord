76d0e3a29b06efa00eb111732ede089e1160dbb3
CorruptX <corrupt2112@gmail.com>
2021-08-02 14:53:32 -0500

    Updated use.js

--- commands/use.js
+++ commands/use.js
@@ -1,16 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args, prefix, player, db) => {
-  let now = new Date();
-  let startembed = new Discord.MessageEmbed()
-  .setTitle('Disabled command')
-  .setDescription('This command is temporarily disabled due to maintenance. Join our [discord server!](https://discord.com/invite/42vkvZMv7d)')
-  .setColor("#fa2c2c")
-  .setTimestamp()
-  message.channel.createMessage({embed:startembed});
-}
-
-/*module.exports.run = async (client, message, args, prefix, player, db) => {
   let stats = await db.collection("users").findOne({_id: message.author.id});
   if(!args[0]) return message.reply("please provide the index number of the item you wish to use.");
   let itemi = stats.items[parseInt(args[0])-1];
@@ -19,7 +9,7 @@ module.exports.run = async (client, message, args, prefix, player, db) => {
   if(!itemc) return message.reply("it looks like you got an unknown item. Please report it at the support server.");
   let item = new itemc(itemi);
   let res = await item.use(client, message, args, prefix, player, db, parseInt(args[0])-1);
-}*/
+}
 
 module.exports.help = {
   name: "use",
